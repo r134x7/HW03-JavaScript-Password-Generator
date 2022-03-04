30507248c23c2438c40f3893206c19af609e17b1
r134x7 <98866524+r134x7@users.noreply.github.com>
2022-03-04 23:21:29 +1100

    called function to get it working, made array using for loop

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,44 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() { // function
-  var password = generatePassword(); // function expression stored in a variable
-    console.log("test");
+  var password = generatePassword(); { // function expression stored in a variable
+  
+    var passwordText = document.querySelector("#password");
 
-  var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+    // console.log(passwordText.value)
+  };
+}
 
-  passwordText.value = password;
+ // cannot seach numbers for some reason
 
+// idea, make for loop that iterates from 8 to 128 and then turn the array numbers into string
+
+var options = []; // creates empty array
+
+for (var i = 8; i < 129; i++) {
+    // options = options + i; this is wrong because it somehow created a 277 length array when i = 0
+    options.push(i); // this is right, creates a 120 length array
+}
+
+function generatePassword() {
+  var userChoice = Number(prompt("How many characters do you want your password to have?"));
+  //userChoice.
+
+  if (!options.includes(userChoice)) {
+    alert("Pick between 8 to 128 characters not " + userChoice)
+    generatePassword();
+    return;
+  }
+
+  if (confirm("Do you want special characters in your password?")) {
+    return 
+  }
+
+// var index = Math.floor(Math.random() * options.length);
+// var computerChoice = options[index]
+
+  return
 }
 
 // Add event listener to generate button
@@ -18,3 +49,4 @@ generateBtn.addEventListener("click", writePassword);
 // first error found in browser: 
 // Uncaught ReferenceError: generatePassword is not defined
 //    at HTMLButtonElement.writePassword (script.js:6:18)
+// solution: it seems the solution was to call the function
