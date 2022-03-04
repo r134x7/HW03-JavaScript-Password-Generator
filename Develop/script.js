4f3b25d44b8f10b64d42999657b127876e9e5424
r134x7 <98866524+r134x7@users.noreply.github.com>
2022-03-05 01:49:26 +1100

    Added confirms with stored values and an alert for no confirm.

--- Develop/script.js
+++ Develop/script.js
@@ -17,6 +17,10 @@ function writePassword() { // function
 // idea, make for loop that iterates from 8 to 128 and then turn the array numbers into string
 
 var options = []; // creates empty array
+var a1 = 0; // lowercase confirm
+var a2 = 0; // uppercase confirm
+var a3 = 0; // numeric confirm
+var a4 = 0; // special confirm
 
 for (var i = 8; i < 129; i++) {
     // options = options + i; this is wrong because it somehow created a 277 length array when i = 0
@@ -25,16 +29,34 @@ for (var i = 8; i < 129; i++) {
 
 function generatePassword() {
   var userChoice = Number(prompt("How many characters do you want your password to have?"));
-  //userChoice.
+  // found solution to the problem where prompt turns a number into a string from https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt 
 
-  if (!options.includes(userChoice)) {
+  if (!options.includes(userChoice)) { 
     alert("Pick between 8 to 128 characters not " + userChoice)
     generatePassword();
     return;
   }
 
+  if (confirm("Do you want lowercase characters in your password?")) {
+    a1 = 1;
+  }
+
+  if (confirm("Do you want uppercase characters in your password?")) {
+    a2 = 1;
+  }
+
+  if (confirm("Do you want numeric characters in your password?")) {
+    a3 = 1;
+  }
+
   if (confirm("Do you want special characters in your password?")) {
-    return 
+    a4 = 1;
+  }
+
+  if (a1 === 0 && a2 === 0 && a3 === 0 && a4 === 0) { 
+    alert("You chose no characters for your password.")
+    generatePassword();
+    return;
   }
 
 // var index = Math.floor(Math.random() * options.length);
