818ea252e64dd68fa2687584f0f79e22c58db79c
r134x7 <98866524+r134x7@users.noreply.github.com>
2022-03-05 17:52:53 +1100

    Created a numeric-only password generator

--- Develop/script.js
+++ Develop/script.js
@@ -16,17 +16,24 @@ function writePassword() { // function
 
 var options = []; // creates empty array
 
-var a1 = 0; // lowercase confirm
-var a2 = 0; // uppercase confirm
-var a3 = 0; // numeric confirm
-var a4 = 0; // special confirm
+// var a1 = 0; // lowercase confirm
+// var a2 = 0; // uppercase confirm
+// var a3 = 0; // numeric confirm
+// var a4 = 0; // special confirm
+
+var ok = { // decided to try out making an object since it's in the course material
+  lowercase: 0,
+  uppercase: 0,
+  numeric: 0,
+  special: 0,
+};
 
 var b1 = "byouldwvzcieahtsnqgxjkrmfp"; // lowercase string
-var b1 = b1.split(""); // splits each charcter into an array
+var b1 = b1.split(""); // splits each character into an array, source recommends not using this method for unicode characters: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
 var b2 = "byouldwvzcieahtsnqgxjkrmfp"; // lowercase string
 var b2 = b2.toUpperCase(); // uppercase string, applying method to an array doesn't work for some reason
-var b2 = b2.split(""); // splits each charcter into an array
+var b2 = b2.split(""); // splits each character into an array
 
 var b3 = [1,2,3,4,5,6,7,8,9,0]; // numeric array
 
@@ -38,10 +45,15 @@ for (var i = 8; i < 129; i++) {
     options.push(i); // this is right, creates a 120 length array
 }
 
+//var index = Math.floor(Math.random() * userChoice.length);
+
+var capture = [];
 
 function generatePassword() {
   var userChoice = Number(prompt("How many characters do you want your password to have?"));
   // found solution to the problem where prompt turns a number into a string from https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt 
+  
+  // console.log(userChoice) // used to confirm local variable is holding number picked
 
   if (!options.includes(userChoice)) { 
     alert("Pick between 8 to 128 characters not " + userChoice)
@@ -50,31 +62,46 @@ function generatePassword() {
   }
 
   if (confirm("Do you want lowercase characters in your password?")) {
-    a1 = 1;
+    // a1 = 1;
+    ok.lowercase++;
   }
 
   if (confirm("Do you want uppercase characters in your password?")) {
-    a2 = 1;
+    // a2 = 1;
+    ok.uppercase++;
   }
 
   if (confirm("Do you want numeric characters in your password?")) {
-    a3 = 1;
+    // a3 = 1;
+    ok.numeric++;
   }
 
   if (confirm("Do you want special characters in your password?")) {
-    a4 = 1;
+    // a4 = 1;
+    ok.special++;
   }
 
-  if (a1 === 0 && a2 === 0 && a3 === 0 && a4 === 0) { 
+  if (ok.lowercase === 0 && ok.uppercase === 0 && ok.numeric === 0 && ok.special === 0) { // forgot to change variables here from a1-a4 to ok
     alert("You chose no characters for your password.")
     generatePassword();
     return;
   }
 
-// var index = Math.floor(Math.random() * options.length);
-// var computerChoice = options[index]
+  if (ok.lowercase === 0 && ok.uppercase === 0 && ok.numeric === 1 && ok.special === 0) {
+    
+    for (var i = 0; i < userChoice; i++) {
 
-  return
+      var test = Math.floor(Math.random() * b3.length);
+      capture.push(b3[test]);
+      // console.log(test) // checks that it works
+      // console.log(capture) // checks that it works
+    }
+    
+    
+  }
+
+  capture = capture.join("") // joins the array elements and turns it into a string, the opposite of split. Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/joinhttps://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+  return capture;
 }
 
 // Add event listener to generate button
